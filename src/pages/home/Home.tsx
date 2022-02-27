ad5a9327f3c3bc433629997e41f586aa9dfcc542
SiewLeng <siewleng.lim@gmail.com>
2022-02-27 17:52:10 +0800

    first commit

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export class Home extends React.Component {
+	render() {
+		return (
+			<div>
+			  <h2> This is Home page </h2>
+				<div>
+					<Link to='/counter'> Counter Page </Link>
+				</div>
+			</div>
+		)
+	}
+}
